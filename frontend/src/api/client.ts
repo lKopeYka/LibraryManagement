6624beb203af44d89e371009e9838e4e2d34c6cf
KopeYka <mihailamont@gmail.com>
2026-05-27 23:41:36 +0300

    commit 28/04/26 11/06

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
+const api: AxiosInstance = axios.create({
   baseURL: '/api',
   headers: { 'Content-Type': 'application/json' }
 });
-
 export default api;
