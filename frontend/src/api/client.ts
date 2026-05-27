17064506439e95f6e4f5dc69fffcbeb29dd7a88b
KopeYka <mihailamont@gmail.com>
2026-05-27 23:55:20 +0300

    commit 28/04/26 11/06

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosInstance } from 'axios';
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
   baseURL: '/api',
