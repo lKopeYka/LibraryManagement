47e04cc4e2eb4c3a0cd85f73da7da59902567149
KopeYka <mihailamont@gmail.com>
2026-05-28 10:17:37 +0300

    commit 28/04/26 11/06

--- src/test/java/com/example/librarymanagement/LibraryManagementApplicationTests.java
+++ src/test/java/com/example/librarymanagement/LibraryManagementApplicationTests.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class LibraryManagementApplicationTests {
-
     @Test
     void alwaysPasses() {
         assertTrue(true);
